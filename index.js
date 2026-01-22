9b2e5bb941022c411c54116a101f41173fb7a1af
Savyez <visnuk252@gmail.com>
2026-01-22 13:26:16 +0530

    Login and Signup forms

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
 
 // Start the server
 app.listen(PORT, () => {
